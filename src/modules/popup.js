afd3f0851ad4506bf939b5d8c71f862964eaebe9
Bondok6 <kirolousbondok6@gmail.com>
2022-12-06 01:29:20 +0200

    Allows options to be accessed through the keyboard

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -1,10 +1,13 @@
 const popup = document.querySelector('.popup');
+const popOptions = [...document.querySelectorAll('.popup__item')];
 let flag = false;
+let active = -1;
 
 const createPopup = () => {
   if (flag) return;
   popup.classList.remove('hidden');
   flag = true;
+  active = -1;
 };
 
 const removePopup = () => {
@@ -14,5 +17,24 @@ const removePopup = () => {
     flag = false;
   }
 };
+
+document.addEventListener('keydown', (e) => {
+  if (!flag) return;
+  if (e.key === 'ArrowDown' && active < popOptions.length - 1) {
+    active += 1;
+    popOptions[active].focus();
+  }
+  if (e.key === 'ArrowUp' && active > 0) {
+    active -= 1;
+    popOptions[active].focus();
+  }
+  if (e.key === 'Escape') removePopup();
+});
+
+// remove popup when clicking outside
+document.addEventListener('click', (e) => {
+  if (e.target.closest('.popup')) return;
+  removePopup();
+});
 
 export { createPopup, removePopup };
